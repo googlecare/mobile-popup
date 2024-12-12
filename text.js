b54f6ffbd8e9ede78728bd941cba367403b5a381
googlecare <gosoxohyeah@gmail.com>
2024-12-12 20:43:29 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1(866) 581-6106 to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1(816) 639-2640 to unlock it!';
